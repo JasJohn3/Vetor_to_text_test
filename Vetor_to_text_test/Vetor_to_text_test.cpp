68ec9559c35b961d87bff60e25cd46ebad170782
JASON JOHNSON <JASJOHN3@uat.edu>
2018-12-16 17:16:36 -0700

    Error in the find function always returning true.

--- Vetor_to_text_test/Vetor_to_text_test.cpp
+++ Vetor_to_text_test/Vetor_to_text_test.cpp
@@ -8,48 +8,60 @@
 #include <fstream>
 #include <algorithm>
 
+
 using namespace std;
 int main()
 {
 	string user_input;
-	vector<string> user_names;
+	vector<string> user_data;
 	vector<string>::iterator iterator;
 	bool exit = true;
 
 	do
 	{
-		char enter_another_name;
+		char Leave_Loop;
 		cout << "Please Enter a user name: " << flush;
-		cin >> user_input;
-		user_names.push_back(user_input);
+		cin>>user_input;
+		user_data.push_back(user_input);
 
-		if (find(user_names.begin(),user_names.end(),user_input)!=user_names.end())
+
+		/*
+
+		function does not work.  Removing from the program to continue work.  
+
+		Function was designed to search through the Vector user_data and compare if the user input already exists.
+
+		The function worked correctly the first time it was compiled and now no longer works.  I am removing to continue with the build.
+
+		if (find(user_data.begin(), user_data.end(), user_input)!=user_data.end())
 		{
-			user_names.pop_back();
-			cout << "That Username already exists." << endl;
+			user_data.pop_back();
+			cout << "User Name already Exists" << endl;
 		}
-		else
+		else{cout << "New User Name Created: " << user_input << endl;}
+
+		Function always returns true.  Uncertain as to why?
+		iterator = find(user_data.begin(), user_data.end(), user_input);
+		if ( iterator != user_data.end())
 		{
-			cout << "New User Name created: " << user_input << endl;
+			user_data.pop_back();
+			cout << "User Name already Exists" << endl;
 		}
+		lse { cout << "New User Name Created: " << user_input << endl; }
 		
-
-		if (find(user_names.begin(),user_names.end(),user_input)!=user_names.end())
-		{
-			user_names.pop_back();
-			cout << "That Username already exists." << endl;
-		}
-		else
-		{
-			cout << "New User Name created: " << user_input << endl;
-		}
+		Third and final attempt.  Erases all data in the vector.
+		auto it = find(user_data.begin(), user_data.end(), user_input);
+		if (it != user_data.end())
+			user_data.erase(it);
+		
+		*/
 		cout << "Would you like to enter another name (Y or N): " << flush;
-		cin >> enter_another_name;
-		if (enter_another_name=='y'|| enter_another_name == 'Y')
+		cin >> Leave_Loop;
+		if (Leave_Loop=='y'|| Leave_Loop == 'Y')
 		{
 			exit = false;
 		}
-		else if (enter_another_name == 'n' || enter_another_name == 'N')
+		else if (Leave_Loop == 'n' || Leave_Loop == 'N')
 		{
 			exit = true;
 		}
@@ -58,7 +70,7 @@ int main()
 
 
 	} while (exit!=true);
-	for (iterator = user_names.begin(); iterator != user_names.end(); iterator++)
+	for (iterator = user_data.begin(); iterator != user_data.end(); iterator++)
 	{
 		cout << *iterator  << endl;
 	}
